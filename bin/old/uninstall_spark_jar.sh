c7cd2ee53c59f26f5cf5a7e6c7e67d1b94d00818
Mr.Tang <geektcp@163.com>
2023-04-17 15:38:16 +0800

    add shell script to release

---
+++ bin/old/uninstall_spark_jar.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+INSTALL_DIR=/root/sparkjar
+INSTALL_DIR_RAW=${INSTALL_DIR}/raw
+INSTALL_DIR_HOTFIX=${INSTALL_DIR}/hotfix
+SPARK_JAR_DIR=/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars
+SPARK_JAR_NAME=spark-sql_2.11-2.4.0-cdh6.2.0.jar
+HDFS_JAR_PATH=hdfs:///tmp/jar/raw/spark-sql_2.11-2.4.0-cdh6.2.0.jar
+BACKUP-SEQ=`date "+%Y.%m.%d-%H.%M.%S"`
+
+mkdir -p ${INSTALL_DIR_RAW}
+mkdir -p ${INSTALL_DIR_HOTFIX}
+
+mv ${SPARK_JAR_DIR}/${SPARK_JAR_NAME} ${INSTALL_DIR_RAW}/${SPARK_JAR_NAME}--BACKUP-SEQ
+md5sum ${INSTALL_DIR_RAW}/${SPARK_JAR_NAME}
+mkdir -p ${INSTALL_DIR_HOTFIX}
+cd ${INSTALL_DIR_RAW}
+hadoop fs -get ${HDFS_JAR_PATH} ${SPARK_JAR_NAME}--BACKUP-SEQ
+
+if [ -f ${SPARK_JAR_DIR}/${SPARK_JAR_NAME} ];then
+    echo "${INSTALL_DIR_RAW}/${SPARK_JAR_NAME} exist"
+	exit 20
+fi
+
+cp ${INSTALL_DIR_HOTFIX}/${SPARK_JAR_NAME}--BACKUP-SEQ ${SPARK_JAR_DIR}/${SPARK_JAR_NAME}
+md5sum ${SPARK_JAR_DIR}/${SPARK_JAR_NAME}
+
---
+++ bin/uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# date: Mon Apr 17 15:32:36 CST 2023
+# author: Mr.Tang
+
+HDFS_JAR_RAW=hdfs:///tmp/jar/raw/spark-sql_2.11-2.4.0-cdh6.2.0.jar
+SPARK_JAR=/opt/cloudera/parcels/CDH-6.2.0-1.cdh6.2.0.p0.967373/jars/spark-sql_2.11-2.4.0-cdh6.2.0.jar
+SPARK_JAR_NAME=spark-sql_2.11-2.4.0-cdh6.2.0.jar
+
+if [ -f ${SPARK_JAR} ];then
+    echo "${SPARK_JAR} exist"
+    md5sum ${SPARK_JAR}
+	rm -rf ${SPARK_JAR}
+else
+    echo "${SPARK_JAR} not exist"
+    exit 10
+fi
+
+rm -rf ./${SPARK_JAR_NAME}
+hadoop fs -get ${HDFS_JAR_RAW}
+mv ./${SPARK_JAR_NAME} ${SPARK_JAR}
+md5sum ${SPARK_JAR}
+
